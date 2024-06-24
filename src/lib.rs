135e715f439ac19d8c5733729f4a07e5d2e9c450
Lilith645 <lilith@inet-sys.com>
2024-06-25 08:51:54 +1000

    fixed logger?

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,24 @@ mod modules;
 
 use crate::modules::generic_message::*;
 
+use log::{Level, Metadata, Record};
+
+struct SimpleLogger;
+
+impl log::Log for SimpleLogger {
+  fn enabled(&self, metadata: &Metadata) -> bool {
+    metadata.level() <= Level::Info
+  }
+
+  fn log(&self, record: &Record) {
+    if self.enabled(record.metadata()) {
+      println!("{} - {}", record.level(), record.args());
+    }
+  }
+
+  fn flush(&self) {}
+}
+
 pub use crate::modules::{
   generic_message::Reward,
   messages::MessageType,
