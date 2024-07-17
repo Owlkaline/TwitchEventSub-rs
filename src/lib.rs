2e73c1146dc040510fd456b1f13b8ff7740f04e8
Lilith645 <lilith@inet-sys.com>
2024-07-18 03:34:01 +1000

    reconnecting confirmed works

--- src/lib.rs
+++ src/lib.rs
@@ -701,6 +701,8 @@ impl TwitchEventSubApi {
     mut twitch_keys: TwitchKeys,
     save_locations: Option<(String, String)>,
   ) {
+    let mut is_reconnecting = false;
+
     loop {
       let client = client.clone();
       let mut client = client.lock().unwrap();
@@ -737,50 +739,52 @@ impl TwitchEventSubApi {
             EventMessageType::Welcome => {
               let session_id = message.clone().payload.unwrap().session.unwrap().id;
 
-              let mut sub_data = subscriptions
-                .iter()
-                .filter_map(|s| s.construct_data(&session_id, &twitch_keys))
-                .filter_map(|s| serde_json::to_string(&s).ok())
-                .collect::<Vec<_>>();
-              sub_data.append(&mut custom_subscriptions);
-
-              info!("Subscribing to events!");
-              let mut clone_twitch_keys = twitch_keys.clone();
-              if let Some(TokenAccess::User(ref token)) = twitch_keys.access_token {
-                sub_data
+              if !is_reconnecting {
+                let mut sub_data = subscriptions
                   .iter()
-                  .map(|sub_data| {
-                    //println!("{:?}", sub_data);
-                    TwitchHttpRequest::new(SUBSCRIBE_URL)
-                      .full_auth(token.to_owned(), twitch_keys.client_id.to_string())
-                      .json_content()
-                      .is_post(sub_data)
-                      .run()
-                  })
-                  .map(|a| {
-                    TwitchEventSubApi::regen_token_if_401(
-                      a,
-                      &mut clone_twitch_keys,
-                      &save_locations,
-                    )
-                  })
-                  .filter_map(Result::err)
-                  .for_each(|error| {
-                    error!("{:?}", error);
-                    message_sender
-                      .send(ResponseType::Error(error))
-                      .expect("Failed to send error Message back to main thread.");
-                  });
-              } else {
-                let _ = message_sender.send(ResponseType::Error(
-                  EventSubError::InvalidAccessToken(format!(
-                    "Expected TokenAccess::User(TOKENHERE) but found {:?}",
-                    twitch_keys.access_token
-                  )),
-                ));
-              }
+                  .filter_map(|s| s.construct_data(&session_id, &twitch_keys))
+                  .filter_map(|s| serde_json::to_string(&s).ok())
+                  .collect::<Vec<_>>();
+                sub_data.append(&mut custom_subscriptions);
 
-              twitch_keys = clone_twitch_keys;
+                info!("Subscribing to events!");
+                let mut clone_twitch_keys = twitch_keys.clone();
+                if let Some(TokenAccess::User(ref token)) = twitch_keys.access_token {
+                  sub_data
+                    .iter()
+                    .map(|sub_data| {
+                      //println!("{:?}", sub_data);
+                      TwitchHttpRequest::new(SUBSCRIBE_URL)
+                        .full_auth(token.to_owned(), twitch_keys.client_id.to_string())
+                        .json_content()
+                        .is_post(sub_data)
+                        .run()
+                    })
+                    .map(|a| {
+                      TwitchEventSubApi::regen_token_if_401(
+                        a,
+                        &mut clone_twitch_keys,
+                        &save_locations,
+                      )
+                    })
+                    .filter_map(Result::err)
+                    .for_each(|error| {
+                      error!("{:?}", error);
+                      message_sender
+                        .send(ResponseType::Error(error))
+                        .expect("Failed to send error Message back to main thread.");
+                    });
+                } else {
+                  let _ = message_sender.send(ResponseType::Error(
+                    EventSubError::InvalidAccessToken(format!(
+                      "Expected TokenAccess::User(TOKENHERE) but found {:?}",
+                      twitch_keys.access_token
+                    )),
+                  ));
+                }
+                twitch_keys = clone_twitch_keys;
+              }
+              is_reconnecting = false;
             }
             EventMessageType::KeepAlive => {
               //println!("Keep alive receive message sent, !implemented");
@@ -795,6 +799,7 @@ impl TwitchEventSubApi {
                 .reconnect_url
                 .unwrap();
 
+              is_reconnecting = true;
               *client = ClientBuilder::new(&url)
                 .unwrap()
                 .add_protocol("rust-websocket-events")
